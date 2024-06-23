71a6be0c4c958960789631106cd20c6c1f8f9a5b
Dario <dario.extra.work@gmail.com>
2024-06-23 16:00:44 +0200

    fix: NO CLUSTERING - Vercel dont support websocket

--- src/app.js
+++ src/app.js
@@ -8,22 +8,8 @@ const bookRouter = require('./routes/book.route');
 const authRouter = require('./routes/auth.route');
 //const User = require('./models/userModel'); 
 
-if (cluster.isMaster){
-    console.log(`Master ${process.pid} is running`);
-    // User.sync().then(() => {
-    //     console.log('User Table syncronized!'); 
-    //     });
 
-    for (let i=0; i < numCPUs; i++ ){
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal)=>{
-        console.log(`Worker ${worker.process.pid} died`);
-        cluster.fork();
-    });
-} else {
-    const app = express();
+const app = express();
     app.use(express.json());
     app.use('/api/user', userRouter);
     app.use('/api/book', bookRouter);
@@ -32,13 +18,12 @@ if (cluster.isMaster){
         resp.send({message: 'API di root ( / ) non implementata!', pid: process.pid})
     });//pid: nuova proprietà dell'oggetto  
 
-    module.exports = app;
+  
     //const sequelize = require('./config/database'); 
 
     const port = config.serverPort || 3000;
     app.listen(port, console.log(`App listening in port ${port}`));
-}
 
-
+    module.exports = app;
 
 
